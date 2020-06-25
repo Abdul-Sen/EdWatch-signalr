57ef4dce17b7e67299db8287831cb59d7d017efb
Abdul <abdul.rm.jahangir@gmail.com>
2020-06-25 07:47:00 -0500

    messagehub service created

--- signalr/Hubs/MessageHub.cs
+++ signalr/Hubs/MessageHub.cs
@@ -1,12 +1,20 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using System;
 
-
-namespace signalr.Hubs {
-    public class MessageHub: Hub{
-        public Task SendMessageToAll(string message){
-            return Clients.All.SendAsync("RecieveMessage",message);
+namespace signalr.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            Console.WriteLine("Invoked sendmessagetoall");
+            Console.WriteLine(Context.ConnectionId);
+            return Clients.All.SendAsync("RecieveMessage", message);
         }
-
+        public Task NewMessageToAll(string payload)
+        {
+            return Clients.AllExcept(Context.ConnectionId).SendAsync("ReciveNewMessage", payload);
+        }
     }
 }
